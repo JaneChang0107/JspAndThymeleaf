a288d1f5681dac2735d99832d54425dc5e676106
ccy <chunchi.yen@gmail.com>
2020-04-10 11:41:18 +0800

    add vue folder and thhome.html thhome.jsp

--- thjsp2/src/main/java/com/thinkpower/controller/thjspController.java
+++ thjsp2/src/main/java/com/thinkpower/controller/thjspController.java
@@ -13,11 +13,18 @@ public class thjspController {
 		model.addAttribute("welcome", "這是Thymeleaf網頁");
 		model.addAttribute("today",new Date());
 		return "th/thhome";
-	}
+	}	
 	@RequestMapping("/jsp")
 	public String toJspHome(Model model) {
 		model.addAttribute("welcome", "這是Jsp網頁");
 		model.addAttribute("today",new Date());
 		return "jsp/jsphome";
+	}
+	@RequestMapping("/vue")
+	public String toVueHome(Model model) {
+		String welcome="Spring框架是 Java 平台的一個開源的全棧（Full-stack）應用程式框架和控制反轉容器實現，一般被直接稱為 Spring";
+		model.addAttribute("welcome", welcome);
+		model.addAttribute("today",new Date());
+		return "vue/thhome";
 	}
 }
